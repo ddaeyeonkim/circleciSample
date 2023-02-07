0c994c46fdf4e5ab5d94e36f01401107b3464063
ddaeyeonkim <improve777@gmail.com>
2023-02-07 18:24:59 +0900

    Add sample native code

--- app/src/main/java/com/example/circlecisample/MainActivity.kt
+++ app/src/main/java/com/example/circlecisample/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.circlecisample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.circlecisample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textview.text = stringFromJNI()
+    }
+
+    external fun stringFromJNI(): String
+
+    companion object {
+        init {
+            System.loadLibrary("circlecisample")
+        }
     }
 }
